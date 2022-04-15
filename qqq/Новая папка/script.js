aabf76a3d4e02e5a1a5ec32ff2c7e2818bacd1ee
Виктория Родионова <92617148+Vkloune@users.noreply.github.com>
2022-04-15 20:37:54 +0500

    Add files via upload

--- qqq/Новая папка/script.js
+++ qqq/Новая папка/script.js
@@ -6,72 +6,80 @@ let calc = {
     vvd: null,
 }
 
-const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-const actions = ['plus', 'mine', 'divi', 'mult',]
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ];
+const actions = ['plus', 'mine', 'div', 'mult'];
 
-$(document).ready(function() {
+
+$(document).ready(function(){
     console.log(calc);
-
     for (let i = 0; i < numbers.length; i++) {
-    }
-
-    for (let i = 0; i < numbers.length; i++) {
-        const e =numbers [i];
-
-        $("#" + e).click(function() {
-            console.log("#" + e);
-    
-            if (!calc.act) {
+        const e = numbers[i];
+        $("#" + e).click(function(){
+            console.log('#' + e);
+            // if (calc.act === null){
+            if (!calc.act){
                 calc.numbA += e;
-                showVvd(calc.numbA);
-            } else {
+                ShowVvd(calc.numbA);
+            } else{
                 calc.numbB += e;
-                showVvd(calc.numbB);
-            }   
-            
+                ShowVvd(calc.numbB);
+            }
+            // calc.numbA = 1;
             console.log(calc);
         });
     }
-  
-    $("#plus").click(function() {
-        console.log("#plus");
-
-        calc.act = "plus";
-
-        console.log(calc); 
-    });      
     
-    $("#rvn").click(function() {
-        console.log("#rvn");
-
-        calc.isRvn = true;
-
-        if (calc.act === 'plus') {
-            calc.Vvd = Number(calc.numbA) + Number(calc.numbB);
-        }
-
-      showVdd(calc.vvd);
-        console.log(calc);  
-    }); 
-
-    function showVvd(number) { 
-        $("#vvd").text(number);   
+    for (let i = 0; i < actions.length; i++) {
+        const e = actions[i];
+        $("#" + e).click(function(){
+            console.log('#' + e);
+            calc.act = e;
+            console.log(calc);
+        });
     }
-    
-    console.log
 
-    $("#2").click(function() {
-        console.log("#1");
+    // $("#2").click(function(){
+    //     console.log('#2');
+    //     // if (calc.act === null){
+    //     if (!calc.act){
+    //         calc.numbA += '2';
+    //         ShowVvd(calc.numbA);
+    //     } else{
+    //         calc.numbB += '2';
+    //         ShowVvd(calc.numbB);
+    //     }
+    //     // calc.numbA = 1;
+    //     console.log(calc);
+    // });
 
-        if (!calc.act) {
-            calc.numbA += '2';
-            showVvd(calc.numbA);
-        } else {
-            calc.numbB += '2';
-            showVvd(calc.numbB);
-        }   
-        
+
+    // $("#plus").click(function(){
+    //     console.log('#plus');
+    //     calc.act = 'plus';
+    //     console.log(calc);
+    // });
+
+    $("#rvn").click(function(){
+        console.log('#rvn');
+        calc.isRvn = true;
+        if (calc.act === 'plus'){
+            calc.vvd = Number(calc.numbA) + Number(calc.numbB);
+        }
+        if (calc.act === 'mine'){
+            calc.vvd = Number(calc.numbA) - Number(calc.numbB);
+        }
+        if (calc.act === 'mult'){
+            calc.vvd = Number(calc.numbA) * Number(calc.numbB);
+        }
+        if (calc.act === 'div'){
+            calc.vvd = Number(calc.numbA) / Number(calc.numbB);
+        }
+        // calc.vvd = calc.numbA + calc.numbB;
+        ShowVvd(calc.vvd);
         console.log(calc);
-    });                                                                     
-});
+    });
 
+    function ShowVvd(number){
+        $("#vvd").text(number);
+    }
+});
